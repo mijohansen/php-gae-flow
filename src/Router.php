fa38e5cf9b2c180b1389eb84e71f38c5cd41894f
Michael Johansen <michael.johansen@nav.no>
2019-10-27 14:48:48 +0100

    Next

--- src/Router.php
+++ src/Router.php
@@ -10,7 +10,7 @@ class Router {
         if (preg_match('/\.(?:png|jpg|jpeg|gif|svg|css|js|map|woff|woff2|ttf|ico|json)$/', $_SERVER["REQUEST_URI"])) {
             return false;    // serve the requested resource as-is.
         } else {
-            $gcloudProject = ComposerExtra::getServeEntrypoint();
+            $gcloudProject = ComposerExtra::getGcloudProject();
             $projectEntryPoint = ComposerExtra::getServeEntrypoint();
             $envLocation = Paths::getUserProjectDir($gcloudProject);
             Dotenv::create($envLocation, Paths::ENV_FILENAME_DEV)->load();

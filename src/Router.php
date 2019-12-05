dc28c2015c60bca7f229c03ed7f8433963c06d0d
Michael Johansen <michael.johansen@gmail.com>
2019-12-05 21:00:24 +0200

    Now supports dotenv 4.0 (#5)

--- src/Router.php
+++ src/Router.php
@@ -6,15 +6,23 @@ use Dotenv\Dotenv;
 
 class Router {
 
+    public static function loadDevEnv() {
+        $gcloudProject = ComposerExtra::getGcloudProject();
+        $envLocation = Paths::getUserProjectDir($gcloudProject);
+        if(method_exists(Dotenv::class,"createImmutable" )){
+            Dotenv::createImmutable($envLocation, Paths::ENV_FILENAME_DEV)->load();
+        }else {
+            Dotenv::create($envLocation, Paths::ENV_FILENAME_DEV)->load();
+        }
+
+    }
+
     public function __invoke() {
         if (preg_match('/\.(?:png|jpg|jpeg|gif|svg|css|js|map|woff|woff2|ttf|ico|json)$/', $_SERVER["REQUEST_URI"])) {
             return false;    // serve the requested resource as-is.
         } else {
-            $gcloudProject = ComposerExtra::getGcloudProject();
-            $projectEntryPoint = ComposerExtra::getServeEntrypoint();
-            $envLocation = Paths::getUserProjectDir($gcloudProject);
-            Dotenv::create($envLocation, Paths::ENV_FILENAME_DEV)->load();
-            return require_once $projectEntryPoint;
+            self::loadDevEnv();
+            return require_once ComposerExtra::getServeEntrypoint();
         }
     }
 }
